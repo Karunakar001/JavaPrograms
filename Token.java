019bbb947209fe00f66b8628b21ded5cdcd8c057
Karunakar001 <103064685+Karunakar001@users.noreply.github.com>
2022-08-29 17:53:51 +0530

    Update Token.java

--- Token.java
+++ Token.java
@@ -3,19 +3,23 @@ package Karna;
 
 import java.util.Arrays;
 import java.util.Scanner;
+import java.util.regex.Pattern;
 
 class Token
 {
     public static void main(String[] args) {
         Scanner scan=new Scanner(System.in);
+        String str=scan.nextLine();
         String input=scan.nextLine();
-        tokenize(input);
+        tokenize(input,str);
         scan.close();
     }
-    public static void tokenize(String input)
+    public static void tokenize(String input,String str)
     {
         String s[]={""};
-        String[] output=input.trim().split("[!?._@'{,},{\s\t}+]+");
+        
+        String[] output=Pattern.compile(str).split(input);//=input.trim().split("[!?._@'{,},{\s\t}+]+");
+        
         if(Arrays.equals(output,s))
         {
             System.out.println("0");
